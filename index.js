e0935c8d53433bd55416c9469cffe7b34a72ddde
hiteshpatil237 <hiteshpatil237@gmail.com>
2024-07-10 14:57:37 -0400

    Added CORS

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 
 import db from "./mongoC.js";
 
 const port = 4000;
 const app = express();
 
-app.use((_req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', '*');
-  
-    next();
-  });
+// Enable CORS for all origins
+app.use(cors());
 
 // Parses the text as url encoded data
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,26 +18,23 @@ app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello World, from express');
-})
+});
 
-app.post('/addUser',async (req, res) => {
+app.post('/addUser', async (req, res) => {
     let collection = await db.collection("users");
     let newDocument = req.body;
     newDocument.date = new Date();
     let result = await collection.insertOne(newDocument);
-    console.log("rreq"+req.body);
-    res.send(result).status(204);
+    console.log("rreq" + req.body);
+    res.status(204).send(result);
 });
 
-app.get('/getUsers', async(req, res) => {
+app.get('/getUsers', async (req, res) => {
     let collection = await db.collection("users");
-    let results = await collection.find({})
-      
-      .toArray();
-    res.send(results).status(200);
+    let results = await collection.find({}).toArray();
+    res.status(200).send(results);
 });
 
 app.listen(port, function () {
     console.log("Server is listening at port:" + port);
 });
- 
